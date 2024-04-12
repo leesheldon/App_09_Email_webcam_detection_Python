6581754bf3e2e4d6a64d51d15906f98b092a67d1
Sheldon <lhanco@gmail.com>
2024-04-12 11:00:49 +0700

    Day 37: Extract image from webcam video.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import glob
 from emailing import send_email
 
 """
@@ -14,6 +15,7 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -46,11 +48,19 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
+        # Extract image from webcam video
+        all_images = sorted(glob.glob("images/*.png"))
+        index = int(len(all_images) / 2)
+        image_with_object = all_images[index]
+        print(f"image with object: {index}.png")
+
         send_email()
 
     cv2.imshow("Video - bounded frame", frame)
@@ -63,59 +73,3 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
