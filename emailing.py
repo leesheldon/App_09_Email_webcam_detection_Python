0242c99d6b5b3ce411c98842b105f900ed0ac068
Sheldon <lhanco@gmail.com>
2024-04-12 11:57:37 +0700

    Day 37: Send email via Gmail with attachment.

--- emailing.py
+++ emailing.py
@@ -1,4 +1,98 @@
+import smtplib
+from PIL import Image
+from io import BytesIO
+import os
+from email.message import EmailMessage
 
-def send_email():
+SENDER = "lhanco@gmail.com"
+PASSWORD = os.getenv("Python_App_Send_Email")
+RECEIVER = "lhanco@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we have just seen a new customer.")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    image_data = BytesIO(content)
+    image_attached = Image.open(image_data)
+    image_format = image_attached.format
+
+    email_message.add_attachment(content,
+                                 filename=f"new_customer.{image_attached.format}",
+                                 maintype="image",
+                                 subtype=image_format)
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
     print("Email was sent!")
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/11.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
