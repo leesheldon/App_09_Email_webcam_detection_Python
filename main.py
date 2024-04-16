d8185e813c46bb8c2aaf815da868ef30d4e336c4
Sheldon <lhanco@gmail.com>
2024-04-16 23:41:06 +0700

    Day 37: Used Lock() to avoid multiple threads from conflicting. Add resume_send_email for any images need resending. Fixed: used image_with_object instead of index.png.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import time
 import glob
 from emailing import send_email
-from threading import Thread
+from threading import Thread, Lock
 from datetime import datetime
 from shutil import copyfile
 
@@ -12,21 +12,24 @@ SENT_OUT_PATH = "sent_out/"
 
 
 def clean_folder():
-    images_list = glob.glob(IMAGES_PATH + "*.png")
-    for image in images_list:
-        os.remove(image)
+    with clean_thread_lock:
+        images_list = glob.glob(IMAGES_PATH + "*.png")
+        for image in images_list:
+            os.remove(image)
 
-    print("Folder was cleaned.")
+        print("Folder was cleaned.")
 
 
 def send_email_then_remove_image(image_param):
-    success, error = send_email(image_param)
-    if success:
-        print("Email was sent!")
+    with email_thread_lock:
+        success, error = send_email(image_param)
+        if success:
+            print("Email was sent!")
 
-        os.remove(image_param)
-    else:
-        print(f"Email sent failed! \n{error}")
+            os.remove(image_param)
+            print("Sent image was removed!")
+        else:
+            print(f"Email sent failed! \n{error}")
 
 
 def put_text_into_video_frame(frame_param):
@@ -38,6 +41,22 @@ def put_text_into_video_frame(frame_param):
     cv2.putText(img=frame_param, text="Press q button to quit.", org=(30, 110), fontFace=cv2.FONT_HERSHEY_PLAIN,
                 fontScale=2, color=(0, 128, 255), thickness=2, lineType=cv2.LINE_AA)
 
+
+def resume_send_email_then_remove_image():
+    all_unsent_images = glob.glob(f"{SENT_OUT_PATH}*.png")
+
+    for unsent_img in all_unsent_images:
+        unsent_img = Thread(target=send_email_then_remove_image, args=(unsent_img,))
+        unsent_img.daemon = True
+        unsent_img.start()
+
+
+# To avoid multiple threads from conflicting when accessing shared resources like files
+email_thread_lock = Lock()
+clean_thread_lock = Lock()
+
+# Call on startup to check which images in "sent_out" folder need resending as attachment.
+resume_send_email_then_remove_image()
 
 """
     VideoCapture(0)
@@ -95,25 +114,30 @@ while True:
         # Extract image from webcam video
         all_images = sorted(glob.glob(IMAGES_PATH + "*.png"))
         index = int(len(all_images) / 2)
-        # image_with_object = all_images[index]
-        print(f"image with object: {index}.png")
+        image_with_object = all_images[index]
+        image_with_object_file_name = os.path.basename(image_with_object)
 
         dt_now = datetime.now()
-        sent_file_name = dt_now.strftime('%H%M%S') + "_" + f"{index}.png"
+        """
+        Ex: image_with_object = "images/17.png"
+        image_with_object_file_name = "17.png"
+        sent_file_name = sent_out/hhmmss_17.png
+        """
+        sent_file_name = SENT_OUT_PATH + dt_now.strftime('%H%M%S') + "_" + image_with_object_file_name
 
         # Copy image with object to sent_out folder to send as attachment via Gmail
-        copyfile(f"{IMAGES_PATH}{index}.png", f"{SENT_OUT_PATH}{sent_file_name}")
+        copyfile(src=image_with_object, dst=sent_file_name)
 
-        clean_folder()
+        clean_folder_thread = Thread(target=clean_folder)
+        clean_folder_thread.daemon = True
+        clean_folder_thread.start()
         # Reset the image frame count
         count = 1
 
-        # Send email with image as attachment
-        all_sent_images = glob.glob(f"{SENT_OUT_PATH}{sent_file_name}")
-        for img in all_sent_images:
-            email_thread = Thread(target=send_email_then_remove_image, args=(img,))
-            email_thread.daemon = True
-            email_thread.start()
+        # Send email with image with object in "sent_out" folder as attachment
+        email_thread = Thread(target=send_email_then_remove_image, args=(sent_file_name,))
+        email_thread.daemon = True
+        email_thread.start()
 
     put_text_into_video_frame(frame)
     cv2.imshow("Video - bounded frame", frame)
@@ -126,8 +150,6 @@ while True:
 
 
 clean_folder()
-for img_sent_out in glob.glob(SENT_OUT_PATH + "*.png"):
-    send_email_then_remove_image(img_sent_out)
-
+print("Quit app!")
 video.release()
 cv2.destroyAllWindows()
